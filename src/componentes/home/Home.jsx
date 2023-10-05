11ddabc55f0f8e83098fbaa23abd8107b2244074
Tu Nombre <agustinrack14@hotmail.com>
2023-10-05 12:12:23 -0300

    edit-home

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import compuImage from "../../assets/Group_47.svg";
+import compuImage from "../../assets/agustinEspalda.png";
 
 export default function Home() {
   return (
@@ -10,12 +10,6 @@ export default function Home() {
           <h1 className="nombre">AGUSTIN RACK</h1>
           <h4 className="subtitulo">DESARROLLADOR FULL-STACK</h4>
           <img src={compuImage} alt="compuImage" className="compu" />
-        </div>
-        <div className="circulos-container">
-          <div className="circulo1"></div>
-        </div>
-        <div className="circulos-container2">
-          <div className="circulo2"></div>
         </div>
       </div>
     </div>

0488f18ebc3671805950732defc0f52beae10258
Tu Nombre <agustinrack14@hotmail.com>
2023-10-10 14:32:25 -0300

    links-the-end

--- src/componentes/aboutMe/AboutMe.jsx
+++ src/componentes/aboutMe/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./AboutMe.css";
 import Button from "react-bootstrap/Button";
+import agustinEspalda from "../../assets/agustinEspalda.png";
 
 function AboutMe() {
   return (
@@ -8,17 +9,25 @@ function AboutMe() {
       <div className="about-contenedor">
         <div className="about-contenido">
           <h1 className="about-titulo">ABOUT ME</h1>
-          <p className="about-descripcion">
-            Developer con actitud positiva y lleno de buenas energías. Siempre
-            dispuesto a seguir aprendiendo para mejorar mis habilidades que me
-            llevan a dar lo mejor de mi. Lo que me caracteriza es la buena
-            comunicacion, lo que me permite relacionarme de manera efectiva con
-            mis colegas y clientes. Mi actitud positiva no solo me ayuda a
-            superar desafíos sino que también crea un ambiente de trabajo
-            agradable.
-          </p>
+          <div className="orden">
+            <img src={agustinEspalda} alt="pcNet" className="pc" />
+            <p className="about-descripcion">
+              Developer con actitud positiva y lleno de buenas energías. Siempre
+              dispuesto a seguir aprendiendo para mejorar mis habilidades que me
+              llevan a dar lo mejor de mi. Lo que me caracteriza es la buena
+              comunicacion, lo que me permite relacionarme de manera efectiva
+              con mis colegas y clientes. Mi actitud positiva no solo me ayuda a
+              superar desafíos sino que también crea un ambiente de trabajo
+              agradable.
+            </p>
+          </div>
           <div className="boton d-flex justify-content-center align-items-center">
-            <Button variant="warning" className="boton">
+            <Button
+              variant="warning"
+              className="boton"
+              href="/assets/CV-agustin.rack.pdf"
+              download="CV-agustin.rack.pdf"
+            >
               DESCARGAR CV
             </Button>
           </div>
